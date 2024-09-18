f81c0f6eac86f96fae0b6e2d3704534deb2667c7
zera2ul <mihail.bereza.ict@gmail.com>
2024-09-19 01:23:21 +0300

    Updated middleware

--- handlers/middleware.py
+++ handlers/middleware.py
@@ -18,8 +18,33 @@ class Middleware(BaseMiddleware):
         event: Message,
         data: Dict[str, Any],
     ) -> Any:
-        if event.content_type == "text" or event.content_type == "users_shared":
-            return await handler(event, data)
+        if event.content_type == "text":
+            if await data["state"].get_state() is None:
+                await handler(event, data)
+            else:
+                commands = [
+                    "/start",
+                    "/help",
+                    "/setutcoffset",
+                    "/feedback",
+                    "/creategroup",
+                    "/addmembers",
+                    "/deletegroup",
+                    "/removemembers",
+                    "/assignreportsrecipient",
+                    "/createreport",
+                    "/getstatistics",
+                    "/getreportsfile",
+                ]
+
+                if not event.text in commands:
+                    return await handler(event, data)
+                else:
+                    mssg_txt = "Вы не можете воспользоваться данной командой во время выполнения другой."
+
+                    await event.answer(mssg_txt)
+        elif event.content_type == "users_shared":
+            await handler(event, data)
         else:
             mssg_txt = "Ваше сообщение не является текстом, отправьте другое."
 
